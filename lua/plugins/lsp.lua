bf84282851d6cfaa937872841e1d09d56963144d
JohnWick92 <grumec92@gmail.com>
2025-07-27 11:47:27 -0300

    Changing vue.lua to lsp.lua and setting css-lsp to work

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,35 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local vue_language_server_path =
+      "/home/shanks/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/"
+    local vue_plugin = {
+      name = "@vue/typescript-plugin",
+      location = vue_language_server_path,
+      languages = { "vue" },
+      configNamespace = "typescript",
+    }
+    local vtsls_config = {
+      settings = {
+        vtsls = {
+          tsserver = {
+            globalPlugins = {
+              vue_plugin,
+            },
+          },
+        },
+      },
+      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+    }
+    local vue_ls_config = {}
+    vim.lsp.config("vtsls", vtsls_config)
+    vim.lsp.config("vue_ls", vue_ls_config)
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+    vim.lsp.config("cssls", {
+      capabilities = capabilities,
+    })
+    vim.lsp.enable({ "vtsls", "vue_ls", "cssls" })
+  end,
+}
